1205a2a790905f36eba589f072a4f7e921f16c5a
Priyank Rajai <priyank73@hotmail.com>
2024-08-12 23:53:55 +0530

    refactor: Summary component

--- src/components/object-stream/index.tsx
+++ src/components/object-stream/index.tsx
@@ -3,43 +3,51 @@
 import { summarySchema } from "@/app/api/use-object/schema";
 import { experimental_useObject as useObject } from "ai/react";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Button } from "../ui/button";
 
 const GenerateSummary = ({ data }: { data: string }) => {
   const { object, submit, isLoading, stop } = useObject({
     api: "/api/use-object",
     schema: summarySchema,
   });
+  const hasSubmitted = useRef(false);
 
   useEffect(() => {
-    if (!data) return;
+    if (!data && hasSubmitted.current) return;
     submit(data);
+    hasSubmitted.current = true;
   }, []);
 
   return (
     <div>
-      <button
-        onClick={() => submit("Messages during finals week.")}
-        disabled={isLoading}
-      >
-        Generate notifications
-      </button>
+      <div>
+        <h2 className="mt-5 font-semibold text-2xl">Summary</h2>
+      </div>
 
       {isLoading && (
         <div>
           <div>Loading...</div>
-          <button type="button" onClick={() => stop()}>
-            Stop
-          </button>
+          <Button variant={"destructive"} onClick={() => stop()}>
+            Stop Generating
+          </Button>
         </div>
       )}
 
-      {object?.data?.map((summary, index) => (
-        <div key={index}>
-          <p>{summary?.title}</p>
-          <p>{summary?.description}</p>
-        </div>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+        {Array.isArray(object?.data) &&
+          object.data.map((summary, index) => (
+            <div key={index}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>{summary?.title}</CardTitle>
+                </CardHeader>
+                <CardContent>{summary?.description}</CardContent>
+              </Card>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
