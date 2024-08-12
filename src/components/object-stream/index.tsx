09707393e3ea6f9ae97fab56409ac69530dab196
Priyank Rajai <priyank73@hotmail.com>
2024-08-12 22:58:50 +0530

    feat: Generate summary card

---
+++ src/components/object-stream/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { summarySchema } from "@/api/use-object/schema";
+import { experimental_useObject as useObject } from "ai/react";
+
+export default function Page() {
+  const { object, submit, isLoading, stop } = useObject({
+    api: "/api/use-object",
+    schema: summarySchema,
+  });
+
+  return (
+    <div>
+      <button
+        onClick={() => submit("Messages during finals week.")}
+        disabled={isLoading}
+      >
+        Generate notifications
+      </button>
+
+      {isLoading && (
+        <div>
+          <div>Loading...</div>
+          <button type="button" onClick={() => stop()}>
+            Stop
+          </button>
+        </div>
+      )}
+
+      {object?.map((summary, index) => (
+        <div key={index}>
+          <p>{summary?.title}</p>
+          <p>{summary?.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
