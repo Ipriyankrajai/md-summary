ed1107d4a37e546cf2c07420659344ff13080e4c
Priyank Rajai <priyank73@hotmail.com>
2024-08-12 22:57:22 +0530

    feat: api end point for generating summary

---
+++ src/api/use-object/route.ts
@@ -0,0 +1,19 @@
+import { openai } from "@ai-sdk/openai";
+import { streamObject } from "ai";
+import { summarySchema } from "./schema";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+    const context = await req.json();
+
+    const result = await streamObject({
+        model: openai("gpt-4o"),
+        schema: summarySchema,
+        prompt:
+            `Generate 10 summary points for this context:` + context,
+    });
+
+    return result.toTextStreamResponse();
+}
